c9b9e8d8670424cb7a91318c36209cab336b01e9
Joaoemanuel19 <joaoemanuelmarinhosousa@gmail.com>
2024-02-23 17:34:19 +0000

    removing products from cart whem clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 2
@@ -30,4 +30,16 @@ export function addToCart(productId) {
       quantity
     })
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem)
+    }
+  });
+
+  cart = newCart;
 }
